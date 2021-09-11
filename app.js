2677365b625e4b66fc7aff32065966a28849ab76
victor <victorifeanyichukwu4@gmail.com>
2021-09-11 12:21:57 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+
+  https.get('https://reqres.in/api/users', (resp) => {
+    let dataReceived = '';
+
+    // called when a data chunk is received.
+    resp.on('data', (chunk) => {
+      dataReceived += chunk;
+    });
+
+    // called when the complete response is received.
+    resp.on('end', () => {
+      console.log(JSON.parse(dataReceived));
+      // res.send(JSON.parse(dataReceived).data[1]);
+      res.send("<img src = " + "https://reqres.in/img/faces/2-image.jpg" + ">");
+
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: ", err.message);
+  });
+
+  // res.sendFile(__dirname + "/index.html");
+
+
+
+
+});
+
+// app.post("/", function (req, res) {
+//   const query = req.body.cityName;
+//   const apiKey = "ca7c312b1af4511855cc6c0fb1bd8e8e";
+//   const units = "metric";
+//   const url =
+//     "https://api.openweathermap.org/data/2.5/weather?q=" +
+//     query +
+//     "&appid=" +
+//     apiKey +
+//     "&units=" +
+//     units;
+
+//   https.get(url, function (response) {
+//     console.log(response.statusCode);
+
+//     response.on("data", function (data) {
+//       const weatherData = JSON.parse(data);
+//       const temp = weatherData.main.temp; // main.temp
+//       const description = weatherData.weather[0].description; // weather[0].description
+//       const icon = weatherData.weather[0].icon; // weather[0].icon
+//       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"; // http://openweathermap.org/img/wn/10d@2x.png
+
+//       res.write("<p>The weather is currently " + description + ".</p>");
+//       res.write(
+//         "<h1>The temperature in " +
+//           query +
+//           " is " +
+//           temp +
+//           " degrees Celcius.</h1>"
+//       );
+//       res.write("<img src=" + imageURL + ">");
+//       res.send();
+
+//       /*
+//       const obj = {
+//         name: "Cindy",
+//         favouriteFood: "Ramen",
+//       };
+//       console.log(JSON.stringify(obj));
+//       */
+//     });
+//   });
+// });
+
+app.listen(port, function () {
+  console.log("Server is running on port " + port);
+});
